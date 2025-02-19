711feaecd46bb24238597e90916e01f6db6a418c
shirish-raj-gupta <codersrg@gmail.com>
2025-02-20 00:59:38 +0530

    created a basic model

--- app.js
+++ app.js
@@ -5,14 +5,28 @@ const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const postModel = require('./models/post');
 const jwt = require('jsonwebtoken');
+const path = require('path');
+const upload = require('./config/config');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', (req,res)=>{
   res.render('index');
+})
+
+app.get('/profile/upload', (req,res)=>{
+  res.render('profileupload');
+})
+
+app.post('/upload', isLoggedIn, upload.single('image'), async(req,res)=>{
+  let user = await userModel.findOne({email: req.user.email});
+  user.profilepic = req.file.filename;
+  await user.save();
+  res.redirect('/profile')
 })
 
 app.post('/register', async(req,res)=>{
@@ -50,7 +64,7 @@ app.get('/like/:id', isLoggedIn ,async(req,res)=>{
   let post = await postModel.findOne({_id: req.params.id}).populate('user');
 
   if(post.likes.indexOf(req.user.userid)=== -1){
-    posts.like.push(req.user.userid);
+    post.likes.push(req.user.userid);
   }
   else{
     post.likes.splice(post.likes.indexOf(req.user.userid),1);
@@ -58,6 +72,36 @@ app.get('/like/:id', isLoggedIn ,async(req,res)=>{
 
  
   await post.save();
+  res.redirect('/profile');
+})
+
+app.get('/profile', isLoggedIn ,async(req,res)=>{
+  let user = await userModel.findOne({email: req.user.email}).populate('posts');
+  res.render('profile' ,{user});
+})
+
+app.get('/like/:id', isLoggedIn ,async(req,res)=>{
+  let post = await postModel.findOne({_id: req.params.id}).populate('user');
+
+  if(post.likes.indexOf(req.user.userid)=== -1){
+    post.likes.push(req.user.userid);
+  }
+  else{
+    post.likes.splice(post.likes.indexOf(req.user.userid),1);
+  }
+
+ 
+  await post.save();
+  res.redirect('/profile');
+})
+
+app.get('/edit/:id', isLoggedIn ,async(req,res)=>{
+  let post = await postModel.findOne({_id: req.params.id}).populate('user');
+  res.render('edit' , {post});
+})
+
+app.post('/update/:id', isLoggedIn ,async(req,res)=>{
+  let post = await postModel.findOneAndUpdate({_id: req.params.id}, {content: req.body.content})
   res.redirect('/profile');
 })
 
